e5534df64035c2bb680b3be7bfd772ea2b8358b7
lavr001 <lavr001@gmail.com>
2017-03-01 15:42:35 -0800

    Geoquake is complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 // define globals
-var monthly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
+var map;
 $(document).on("ready", function() {
+  initMap();
   getData();
 });
 
 function getData () {
+  var monthly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
   $.ajax({
     method: 'GET',
     url: monthly_quakes_endpoint,
@@ -15,13 +18,32 @@ function getData () {
   });
 }
 
+
+function initMap() {
+
+  var myLatLng = {lat: 37.78, lng: -122.44};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: myLatLng
+  });
+
+}
+
 function onSuccess (data) {
   data.features.forEach(function (earthquake) {
     var currentTimeInMs = Date.now();
     var earthquakeTimeInMs = earthquake.properties.time;
     var time = (currentTimeInMs - earthquakeTimeInMs) / (1000*60*60);
     var title = earthquake.properties.title;
-    $('#info').append('<strong>' + title + ' / ' + Math.round(time) + ' hours ago' + '</strong>' + '<br>');
+    new google.maps.Marker({
+      position: new google.maps.LatLng(
+        earthquake.geometry.coordinates[1],
+        earthquake.geometry.coordinates[0]
+      ),
+      map: map,
+      title: earthquake.properties.title
+    });
+    $('#info').append('<strong>' + title + '</strong>' + ' / ' + Math.round(time) + ' hours ago' + '<br>');
   });
 }
 
