15584e1db2703ebcc2c067cbc20ca86973cc393a
lavr001 <lavr001@gmail.com>
2017-03-01 12:30:03 -0800

    Added earthquake titles and time to the page

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,36 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var monthly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 $(document).on("ready", function() {
-
-  // CODE IN HERE!
-
+  getData();
 });
+
+function getData () {
+  $.ajax({
+    method: 'GET',
+    url: monthly_quakes_endpoint,
+    dataType: 'json',
+    success: onSuccess,
+    error: onError
+  });
+}
+
+function onSuccess (data) {
+  data.features.forEach(function (earthquake) {
+    var currentTimeInMs = Date.now();
+    var earthquakeTimeInMs = earthquake.properties.time;
+    var time = (currentTimeInMs - earthquakeTimeInMs) / (1000*60*60);
+    var title = earthquake.properties.title;
+    $('#info').append('<strong>' + title + ' / ' + Math.round(time) + ' hours ago' + '</strong>' + '<br>');
+  });
+}
+
+function onError(xhr, status, errorThrown) {
+ alert("Sorry, there was a problem!");
+ console.log("Error: " + errorThrown);
+ console.log("Status: " + status);
+ console.dir(xhr);
+}
+
+
+
